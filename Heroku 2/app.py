92fc952f8490afed2ea1fda4d7c6dfe4b5a37ff9
Basma Gado <90475214+BasmaG1@users.noreply.github.com>
2022-04-10 10:09:56 -0400

    Sample-Site

---
+++ Heroku 2/app.py
@@ -0,0 +1,93 @@
+from flask import Flask, render_template,request ,redirect, url_for
+import re
+from turtle import bye
+import long_responses as long
+
+app = Flask(__name__)
+
+@app.route('/',methods = ['POST'])
+def postit():  
+    usertxt = request.form['chatbox'] 
+    reply=get_response(usertxt)
+    count=Inc_Counter()
+    return render_template("index.html",reply=reply, count=count)
+
+@app.route('/',methods = ['GET'])
+def index(): 
+    count=Inc_Counter()
+    # Render HTML with count variable
+    return render_template("index.html", count=count)
+
+def Inc_Counter():
+     # Load current count
+    f = open("count.txt", "r")
+    count = int(f.read())
+    f.close()
+
+    # Increment the count
+    count += 1
+
+    # Overwrite the count
+    f = open("count.txt", "w")
+    f.write(str(count))
+    f.close()
+    return count
+
+
+
+# Used to get the response
+def get_response(user_input):    
+    split_message = re.split(r'\s+|[,;?!.-]\s*', user_input.lower())
+    response = check_all_messages(split_message)
+    return response    
+
+def message_probability(user_message, recognised_words, single_response=False, required_words=[]):
+    message_certainty = 0
+    has_required_words = True
+
+    # Counts how many words are present in each predefined message
+    for word in user_message:
+        if word in recognised_words:
+            message_certainty += 1
+
+    # Calculates the percent of recognised words in a user message
+    percentage = float(message_certainty) / float(len(recognised_words))
+
+    # Checks that the required words are in the string
+    for word in required_words:
+        if word not in user_message:
+            has_required_words = False
+            break
+
+    # Must either have the required words, or be a single response
+    if has_required_words or single_response:
+        return int(percentage * 100)
+    else:
+        return 0
+
+
+def check_all_messages(message):
+    highest_prob_list = {}
+
+    # Simplifies response creation / adds it to the dict
+    def response(bot_response, list_of_words, single_response=False, required_words=[]):
+        nonlocal highest_prob_list
+        highest_prob_list[bot_response] = message_probability(message, list_of_words, single_response, required_words)
+
+    # Responses -------------------------------------------------------------------------------------------------------
+    response('Hello!', ['hello', 'hi', 'hey', 'sup', 'heyo'], single_response=True)
+    response('See you!', ['bye', 'goodbye'], single_response=True)
+    response('I\'m doing fine, and you?', ['how', 'are', 'you', 'doing'], required_words=['how'])
+    response('You\'re welcome!', ['thank', 'thanks'], single_response=True)
+
+
+    best_match = max(highest_prob_list, key=highest_prob_list.get)
+    # print(highest_prob_list)
+    # print(f'Best match = {best_match} | Score: {highest_prob_list[best_match]}')
+
+    return long.unknown() if highest_prob_list[best_match] < 1 else best_match   
+
+if __name__ == "__main__":
+    app.run(debug = True)
+    #app.run()
+
